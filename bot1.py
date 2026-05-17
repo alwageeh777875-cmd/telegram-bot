9d04c2cbe7c595243b7bb3519732a6f91b2643a1
alwageeh777875-cmd <alwageeh777875@gmail.com>
2026-05-17 15:15:23 +0300

    Add files via upload

---
+++ bot1.py
@@ -0,0 +1,34 @@
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+
+TOKEN = "8682736647:AAEplBFLa9BcYjt4ijNuJtzekPNp6C2iUqQ"
+
+# عند /start
+async def start(update, context):
+    keyboard = [
+        ["📩 مرحباً", "ℹ️ معلومات"]
+    ]
+
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+    await update.message.reply_text("اختر زر:", reply_markup=reply_markup)
+
+
+# الرد على الأزرار
+async def handle_message(update, context):
+    text = update.message.text
+
+    if text == "📩 مرحباً":
+        await update.message.reply_text("أهلاً بك 👋")
+
+    elif text == "ℹ️ معلومات":
+        await update.message.reply_text("هذا بوت تجريبي 🤖")
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
+print("البوت يعمل...")
+
+app.run_polling()
